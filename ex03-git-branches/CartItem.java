78ca3165d67de4ba2f59214d80b73a512f41eaa9
Dori <denmand@augsburg.edu>
2023-01-24 11:08:57 -0600

    fixed issue2

--- ex03-git-branches/CartItem.java
+++ ex03-git-branches/CartItem.java
@@ -1,12 +1,29 @@
 public class CartItem {
   
-  double Price;
+  
+  Item item;
+  double Subtotal;
   int Quantity;
 
-  public CartItem() {
-    Price = 0.0;
+
+  /*public CartItem() {
+    Subtotal = 0.0;
     Quantity = 0;
+  }*/
+
+  public String cartDescription(){
+    String description = item.getDescription();
+        return description;
   }
+
+  public double price(){
+    item.getPrice();
+   Subtotal = item.getPrice() * Quantity;
+    return Subtotal;
+  }
+
+
+  
 }
 
 
--- ex03-git-branches/Item.java
+++ ex03-git-branches/Item.java
@@ -2,5 +2,17 @@ public class Item {
     private String description;
     private double price;
     private int Quantity;
+
+    public double getPrice(){
+        return price;
+    }
     
+    public String getDescription(){
+        return description;
+    }
+
+    public int getQuantity(){
+        return Quantity;
+    }
+
 }
