51ce3f26d3d6016b15abdcacff12381af5e99248
AdnanAdan <97487396+AdnanAdan@users.noreply.github.com>
2023-01-24 10:41:31 -0600

    fixed issue3-classl-Customer

---
+++ ex03-git-branches/Customer.java
@@ -0,0 +1,31 @@
+public class Customer {
+private String name;
+private String email;
+private int id;
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+
+}
