02d4d0e43714e71899b8ba648d70cbb6f447b291
Dori <denmand@augsburg.edu>
2023-01-24 11:31:00 -0600

    fixed issue5

--- ex03-git-branches/Cart.java
+++ ex03-git-branches/Cart.java
@@ -1,6 +1,8 @@
 // A collection of elements in a cart
 // CartContents "has a" CartEntry - using composition
 public class Cart {
-    int ItemCount; // number of elements in cart
-    CartItem[] Items;
+   // int ItemCount; // number of elements in cart
+    CartItem[] CartItems;
+    double Total;
+    boolean Paid; 
 }
