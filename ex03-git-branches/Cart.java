18c04e121a557d7ad37bed4e935608865bd1dde7
vanegasw <vanegasw@augsburg.edu>
2023-01-25 11:36:21 -0600

    Minor Fixes

--- ex03-git-branches/Cart.java
+++ ex03-git-branches/Cart.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+
 // A collection of elements in a cart
 // CartContents "has a" CartEntry - using composition
 public class Cart {
-   // int ItemCount; // number of elements in cart
-    CartItem[] CartItems;
-    double Total;
-    boolean Paid; 
+    private ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
+    private double total;
+    private boolean paid; 
+
+    public void add(){
+        
+    }
 }
